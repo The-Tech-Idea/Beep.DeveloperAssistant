f9a702a3e4ef0f307439252e7f9b547ea9c5cb2b
fahad@thetechidea.com <fahad@thetechidea.com>
2023-12-22 20:15:34 +0300

    update

--- Beep.DeveloperAssistant.Logic/DeveloperAssistantMenuFunctions.cs
+++ Beep.DeveloperAssistant.Logic/DeveloperAssistantMenuFunctions.cs
@@ -75,7 +75,7 @@ namespace Beep.DeveloperAssistant.Logic
                                                 IBranch br = ExtensionsHelpers.TreeEditor.treeBranchHandler.GetBranch(item);
 
                                                 //         TreeEditor.AddCommentsWaiting($"{i} - Added {br.BranchText} to {Passedarguments.DatasourceName}");
-                                                EntityStructure ent = ExtensionsHelpers.DataSource.GetEntityStructure(br.BranchText, false);
+                                                EntityStructure ent = ExtensionsHelpers.DataSource.GetEntityStructure(br.BranchText, true);
 
                                                 DMEEditor.classCreator.CreateINotifyClass(ent,ent.EntityName,"","", Path.Combine(DMEEditor.ConfigEditor.Config.ScriptsPath, Passedarguments.DatasourceName));
                                                 i += 1;
@@ -165,7 +165,7 @@ namespace Beep.DeveloperAssistant.Logic
                                                 ExtensionsHelpers.Vismanager.PasstoWaitForm(args);
                                                 if (!ExtensionsHelpers.DataSource.Entities.Where(p => p.EntityName.Equals(br.BranchText, StringComparison.OrdinalIgnoreCase)).Any())
                                                 {
-                                                    entity = (EntityStructure)srcds.GetEntityStructure(br.BranchText, false).Clone();
+                                                    entity = (EntityStructure)srcds.GetEntityStructure(br.BranchText, true).Clone();
                                                 }
                                                 else
                                                 {
@@ -277,7 +277,7 @@ namespace Beep.DeveloperAssistant.Logic
                                                 //    DMEEditor.AddLogMessage("Beep", args.ParameterString1, DateTime.Now, 0, Passedarguments.DatasourceName, Errors.Ok);
                                                 if (!ExtensionsHelpers.DataSource.Entities.Where(p => p.EntityName.Equals(br.BranchText, StringComparison.OrdinalIgnoreCase)).Any())
                                                 {
-                                                    entity = (EntityStructure)srcds.GetEntityStructure(br.BranchText, false).Clone();
+                                                    entity = (EntityStructure)srcds.GetEntityStructure(br.BranchText, true).Clone();
                                                 }
                                                 else
                                                 {
@@ -388,7 +388,7 @@ namespace Beep.DeveloperAssistant.Logic
                                         // ExtensionsHelpers.Vismanager.PasstoWaitForm(args);
                                         ExtensionsHelpers.progress.Report(args);
                                         //    DMEEditor.AddLogMessage("Beep", args.ParameterString1, DateTime.Now, 0, Passedarguments.DatasourceName, Errors.Ok);
-                                            entity = (EntityStructure)srcds.GetEntityStructure(br.BranchText, false).Clone();
+                                            entity = (EntityStructure)srcds.GetEntityStructure(br.BranchText, true).Clone();
                                         //     TreeEditor.AddCommentsWaiting($"{i}- Added Entity {entity.EntityName}");
                                         ls.Add(entity);
                                         i++;
